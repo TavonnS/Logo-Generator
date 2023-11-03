94955d1e454c337636e82f2dac774e40f7d1f416
TavonnS <142205891+TavonnS@users.noreply.github.com>
2023-11-02 17:24:15 -0700

    svg

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ inquirer.prompt(prompt)
     console.log(answers);
     // function for writing shape code based on type:
     const shapeChoice = function (answers) {
-        if(answers.shape === 'Circle') {
-            return `circle cx: '150' cy: '100' r: '100' fill: '${answers.shapeColor}'`;
-        } else if (answers.shape === 'Square') {
-            return `rect width: '200px' height: '200px' fill: '${answers.shapeColor}'`;
+        if(answers.shape == 'Circle') {
+            return `circle cx="150" cy="100" r="100" fill="${answers.shapeColor}"`;
+        } else if (answers.shape == 'Square') {
+            return `rect width="200px" height="200px" fill="${answers.shapeColor}"`;
         } else {
-            return `polygon points: '0,0 300,0 150,200' fill: '${answers.shapeColor}'`;
+            return `polygon points="0,0 300,0 150,200" fill="${answers.shapeColor}"`;
         }
     };
    
     const svgMake = 
-    `<svg width: '300px' height: '200px'>
+    `<svg width="300px" height="200px">
         <${shapeChoice(answers.shape)} />
             <text x="50%" y="50%" fill="${answers.textColor}" text-anchor="middle">${answers.text}</text>
     </svg>     
